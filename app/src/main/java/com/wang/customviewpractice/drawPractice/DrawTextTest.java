d0b045f4dc51eb4d2e3349e69fe83f89a053c884
wangwei <2313361086@qq.com>
2017-03-30 23:01:46 +0800

    学习计算baseline的位置

---
+++ app/src/main/java/com/wang/customviewpractice/drawPractice/DrawTextTest.java
@@ -0,0 +1,13 @@
+package com.wang.customviewpractice.drawPractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawTextTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_text_test);
+    }
+}
---
+++ app/src/main/java/com/wang/customviewpractice/drawingofview/DrawTextView.java
@@ -0,0 +1,8 @@
+package com.wang.customviewpractice.drawingofview;
+
+/**
+ * Created by wangdachui on 2017/3/30.
+ */
+
+public class DrawTextView {
+}
